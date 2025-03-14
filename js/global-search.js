f87185eac322649a189c27b5df84562909912037
Yulia Alpatova <blackmothy@yandex.ru>
2025-03-14 14:57:52 +0300

    Добавлена тропа Пятиярви, версия js карты = 9, версия js поиска = 8

--- js/global-search.js
+++ js/global-search.js
@@ -1,4 +1,5 @@
 const trailsData = [
+    { name: "Экотропа Петяярви", url: "/catalog/lenoblast/petyayarvi/" },
     { name: "Танцующий лес", url: "/catalog/kaliningradskaya-oblast/tancuyushchij-les/" },
     { name: "Высота Эфа", url: "/catalog/kaliningradskaya-oblast/vysota-efa/" },
     { name: "Долина Нарзанов", url: "/catalog/kabardino-balkariya/dolina-narzanov/" },
