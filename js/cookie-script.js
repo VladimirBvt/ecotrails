e9ce6200145dee5c55cc7f13b76ce533891e48ef
Vladimir_Bavtenko <batonich84@mail.ru>
2025-04-25 08:07:30 +0300

    Добавлен скрипт создания окна информации о куки, как кастомного html элемента. Добавление данного окна в верстку на страницы главная, /catalog, /rules и /police.

--- js/cookie-script.js
+++ js/cookie-script.js
@@ -1,16 +1,74 @@
-const cookiePopup = document.querySelector('.cookie');
-const acceptButton = document.querySelector('.cookie__button');
-
-acceptButton.addEventListener('click', () => {
-  cookiePopup.style.display = 'none';
-
-  localStorage.setItem('cookieAccepted', 'true');
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-  const isCookieAccepted = localStorage.getItem('cookieAccepted');
-
-  if (isCookieAccepted) {
-    cookiePopup.style.display = 'none';
+class CookiePopupElement extends HTMLElement {
+  constructor() {
+    super();
   }
-});
+
+  render() {
+    const shadow = this.attachShadow({mode: 'open'});
+    const styleHref = this.getAttribute('style-href');
+
+    const wrapper = document.createElement('div');
+    wrapper.setAttribute('class', 'cookie');
+
+    const content = document.createElement('div');
+    content.setAttribute('class', 'cookie__content');
+
+    const textWrapper = document.createElement('div')
+    textWrapper.setAttribute('class', 'cookie__text');
+    const textSpan = document.createElement('span')
+    textSpan.textContent = 'Пользуясь нашим сайтом, вы соглашаетесь с тем, что '
+    const textLink = document.createElement('a')
+    textLink.setAttribute('href', '/policy/');
+    textLink.setAttribute('class', 'text-link');
+    textLink.textContent = 'мы используем cookies';
+
+    const button = document.createElement('button');
+    button.setAttribute('class', 'cookie__button')
+    button.textContent = 'OK';
+
+    const linkElem = document.createElement("link");
+    linkElem.setAttribute("rel", "stylesheet");
+    linkElem.setAttribute("href", styleHref);
+
+    shadow.appendChild(linkElem);
+    shadow.appendChild(wrapper);
+    wrapper.appendChild(content);
+    content.appendChild(textWrapper);
+    content.appendChild(button);
+    textWrapper.appendChild(textSpan);
+    textWrapper.appendChild(textLink);
+  }
+
+  connectedCallback() {
+    this.render();
+
+    const cookiePopup = document.querySelector('cookie-popup').shadowRoot.querySelector('.cookie');
+    const cookieButton = document.querySelector('cookie-popup').shadowRoot.querySelector('.cookie__button');
+
+    document.addEventListener('DOMContentLoaded', () => {
+      const isCookieAccepted = localStorage.getItem('cookieAccepted');
+
+      if (isCookieAccepted) {
+        cookiePopup.style.display = 'none';
+      }
+    });
+
+    cookieButton.addEventListener('click', () => {
+      cookiePopup.style.display = 'none';
+
+      localStorage.setItem('cookieAccepted', 'true');
+      });
+  }
+
+  static get observedAttributes() { // (3)
+    return ['style-href'];
+  }
+}
+
+customElements
+  .define(
+    "cookie-popup"
+    ,
+    CookiePopupElement
+  )
+;
