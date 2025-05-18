4374d3377000953070a59e86f83e657f5b108f1e
Julia <blackmothy@yandex.ru>
2025-05-18 14:54:45 +0300

    Исправлена Мск на Москва

--- catalog/js/regions.js
+++ catalog/js/regions.js
@@ -5,7 +5,7 @@ const regionsData = [
   { value: 'krasnodarsky-krai', name: 'Краснодарский край', shortName: 'Краснодарский край', coordinates: [45.04059, 38.97693] },
   { value: 'nizhegorodskaya-oblast', name: 'Нижегородская область', shortName: 'Нижегородская область', coordinates: [56.32867, 44.00205] },
   { value: 'yaroslavskaya-oblast', name: 'Ярославская область', shortName: 'Ярославская область', coordinates: [57.62608, 39.88447] },
-  { value: 'moskva', name: 'Москва и Московская область', shortName: 'Мск и Мособласть', coordinates: [55.751244, 37.618423] },
+  { value: 'moskva', name: 'Москва и Московская область', shortName: 'Москва и Мособласть', coordinates: [55.751244, 37.618423] },
   { value: 'krym', name: 'Республика Крым', shortName: 'Крым', coordinates: [44.95212, 34.10242] },
   { value: 'stavropolsky-krai', name: 'Ставропольский край', shortName: 'Ставрополье', coordinates: [45.04331, 41.9691] },
   { value: 'kabardino-balkariya', name: 'Кабардино-Балкария', shortName: 'Кабардино-Балкария', coordinates: [43.4846, 43.6072] },  
